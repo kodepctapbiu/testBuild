06aa421b847502a701e7043eeb7b10e7b980a7ac
Petrov Valeriovich Stanislav <ctapbiu_kodep@hotmail.com>
2017-07-09 00:11:22 +0300

    proj init

---
+++ Assets/Editor/Builder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class Builder
+{
+    [RuntimeInitializeOnLoadMethod]
+    public static void TryBuild()
+    {
+        foreach(var arg in Environment.GetCommandLineArgs())
+        {
+            if(arg == "-BuildMyTarget")
+            {
+                var res = BuildPipeline.BuildPlayer(new[] { "1.unity" }, Path.Combine(Path.GetDirectoryName(Application.dataPath), "builds\\1.exe"), BuildTarget.StandaloneWindows64, BuildOptions.None);
+                EditorApplication.Exit(string.IsNullOrEmpty(res) ? 1 : 0);
+            }
+        }
+    }
+}
